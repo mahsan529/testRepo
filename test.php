4aae28e1b122f474fdddefc6fe15e0eae358c516
Ahsan Nadeem <mahsan529@gmail.com>
2016-05-18 15:19:27 +0500

    test

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php echo phpinfo();?> 
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+  <script>tinymce.init({ selector:'textarea' });</script>
+</head>
+<body>
+  <textarea>Easy (and free!) You should check out our premium features.</textarea>
+</body>
+</html>
+
+
+<?php 
+
+?>
